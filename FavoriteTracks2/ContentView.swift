f59bab275cb9080785e6c9667fd5037933a25ccd
BenMontecastro-Tactician <bmontecastro@tactician.com>
2021-09-21 21:08:29 +1000

    Initial Commit

---
+++ FavoriteTracks2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteTracks2
+//
+//  Created by Ben Montecastro on 21/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
